9a90abc5c937a83aaf7560da416d2418d48ce311
Bryce <brycegevans@gmail.com>
2025-08-18 20:15:50 -0700

    home title fix

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -24,11 +24,12 @@ export class Home implements OnInit, OnDestroy {
 
   ngOnInit(): void {
   this.intervalId = setInterval(() => {
-    this.rotatingMessage = ''; // Step 1: Clear the message
+    this.rotatingMessage = ''; 
     setTimeout(() => {
-      // Step 2: After one JS event cycle (~0ms delay)
-      this.roleIndex = (this.roleIndex + 1) % this.roles.length; // Step 3: Increment index (loop around)
-      this.rotatingMessage = this.roles[this.roleIndex];      // Step 4: Set new message
+
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length; 
+      this.rotatingMessage = this.roles[this.roleIndex];
+      this.cdr.detectChanges();
     });
 
   }, 4000); 
